154ebd8069400944fc0ec3d22eb2e54b421dabbb
Bhishma Dedhia <bhishma.dedhia@gmail.com>
2017-06-19 12:13:22 +0530

    Deleted pyc files and renamed code file

---
+++ Doc-scanner/blending.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+__all__=["mb_blending"]
+
+def mb_blending(images):
+	GPA=[]
+	for i in range(len(images)):
+		g=images[i].copy()
+		gpi=[g]
+		for t in range(6):
+			a=cv2.pyrDown(gpi[t])
+			
+			gpi.append(a)
+		print gpi[0].shape[0]
+
+		GPA.append(gpi)
+
+	LPA=[]
+	for i in range(len(images)):
+		gpi=GPA[i]
+		lpi=[gpi[5]]
+		for t in range(5,0,-1):
+			GE=cv2.pyrUp(gpi[t])
+			xoff=GE.shape[1]-gpi[t-1].shape[1]
+			yoff=GE.shape[0]-gpi[t-1].shape[0]
+			GE=GE[yoff:,xoff:]
+			l=cv2.subtract(gpi[t-1],GE)
+			print l.shape[0]
+			lpi.append(l)
+		LPA.append(lpi)
+
+
+	Ladded=[]
+	for i in range(len(LPA[0])):
+		ls=LPA[0][i]
+		for t in range(1,len(LPA)):
+			ls=np.add(ls,LPA[t][i])
+		Ladded.append(ls)
+
+	ls_=Ladded[0]
+	for i in range(1,6):
+		ls_=cv2.pyrUp(ls_)
+		xoff=ls_.shape[1]-Ladded[i].shape[1]
+		yoff=ls_.shape[0]-Ladded[i].shape[0]
+		ls_=ls_[yoff:,xoff:]
+		ls_=cv2.add(ls_,Ladded[i])
+	return ls_
+
+
+
+
+
+
+
--- Doc-scanner/testcase.py
+++ Doc-scanner/testcase.py
@@ -1,22 +1,23 @@
 import cv2
 from matplotlib import pyplot as plt
 from ORBandFeaturematching import*
-from Homography import*
+from homography import*
 import numpy as np
 from numpy import linalg as lin
-from Averaging import*
-from Blending import*
-from Image_alignment import*
+from averaging import*
+from blending import*
+from image_alignment import*
 
-img1=cv2.imread('cover1.jpg',0)
-img2=cv2.imread('cover2.jpg',0)
-img3=cv2.imread('cover3.jpg',0)
-img4=cv2.imread('cover4.jpg',0)
+img1=cv2.imread('1.jpg',0)
+img2=cv2.imread('2.jpg',0)
+img3=cv2.imread('3.jpg',0)
+img4=cv2.imread('4.jpg',0)
 images=[img1,img2,img3,img4]
-'''for i in range(len(images)):
-	images[i]=cv2.resize(images[i],None,fx=0.25,fy=0.25,interpolation = cv2.INTER_CUBIC)'''
+for i in range(len(images)):
+	images[i]=cv2.resize(images[i],None,fx=0.25,fy=0.25,interpolation = cv2.INTER_CUBIC)
 
 transformedimg=Image_Align(images)
+'''transformedimg=averaging(transformedimg)'''
 image=mb_blending(transformedimg)		
 '''for i in range(len(transformedimg)):
     image=np.add(image,transformedimg[i])'''
@@ -25,6 +26,7 @@ image=np.add(image,transformedimg[2])'''
 
 plt.imshow(image,cmap='gray')
 plt.show()
+
 '''
 plt.subplot(221),plt.imshow(transformedimg[0],cmap='gray')
 plt.subplot(222),plt.imshow(transformedimg[1],cmap='gray')
